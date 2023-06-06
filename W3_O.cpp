d9bea6f5a08a54c611f448e3439be5d22810a86e
GrainSack <91248817+GrainSack@users.noreply.github.com>
2023-06-06 18:51:54 +0900

    Add files via upload

---
+++ W3_O.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF = 987654321;
+int n,h,m,a,b, ret = INF,visited[34][34];
+
+
+bool check(){
+	for(int i = 1; i<=n;i++){
+		int start = i;
+		for(int j = 1; j<=m;j++){
+			if(visited[j][start]){
+				start++;
+			}
+			else if(visited[j][start-1]){
+				start--;
+			}
+		}
+		if(start!=i){
+			return false;
+		}
+	}
+	return true;
+}
+
+void go(int here, int cnt){
+		if(cnt >3|| cnt >= ret){
+			return;
+		}
+		if(check()){
+			ret = min(ret,cnt); return;
+		}
+		for(int i = here; i<= m;i++){
+			for(int j = 1; j<=n;j++){
+				if(visited[i][j]||visited[i][j-1]||visited[i][j+1])continue;
+				visited[i][j]=1;
+				go(i, cnt+1);
+				visited[i][j]=0;
+			}
+		}
+	return ;
+}
+
+main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+	cin>>n>>h>>m;
+	for(int i=0; i<h; i++){
+		cin>>a>>b;
+		visited[a][b]=1;
+	}
+	go(1,0);
+	cout<<((ret==INF) ? -1:ret)<<"\n";
+	return 0;
+}
